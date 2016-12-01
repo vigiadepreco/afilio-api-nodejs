0ace5a72c265ab4635cdef5a6870fdf44f9affc4
Andre <Andre@DESKTOP-EVCUACT>
2016-12-01 00:48:16 -0200

    fix report function

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ module.exports = function(token, affid, siteid){
          */
         report: function(datestart, dateend, cb){
             this.getinapi("http://v2.afilio.com.br/api/leadsale_api.php?mode=list&type=sale&dateType=transaction&format=JSON&token=" + token + "&affid=" + affid + "&dateStart=" + datestart + "&dateEnd=" + dateend, cb);
-        }
+        },
         
         /**
          * Create tracking links
